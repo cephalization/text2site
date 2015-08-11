f033e050507eaa2c216a8e973f562c0001700f61
Aytu5 <ajdavid@mtu.edu>
2015-08-11 12:14:22 -0400

    Updated compile.bat

--- compile.bat
+++ compile.bat
@@ -1,8 +1,7 @@
 @echo off
 cmd /c set path=%path%;"C:\Program Files\Java\jdk1.8.0_51\bin\;"
-javac -g Text2SiteMain.java
+javac -g Text2SiteMain.java FileMaker.java
 mkdir text2site
-move Text2SiteMain.class text2site/
+move *.class text2site/
 jar cfm text2site.jar manifest.txt text2site/*.class
 rmdir /s /q text2site
-
